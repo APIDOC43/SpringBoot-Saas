956833fb7924f440810f36ea699484edc5d838b7
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-11-06 20:20:09 +0900

    🔨FEAT(api-dependency): mongoDB 저장 로직 구현 #13
    api-dependency File (구 output.yaml) 저장 후 mongoDB Collection에 저장합니다.
    file
    save 에서 mongoDB save로 완전히 변경 후 File 저장은 제거해야합니다.

---
+++ src/main/java/com/hocs/server/extractor/domain/AOP.java
@@ -0,0 +1,25 @@
+package com.hocs.server.extractor.domain;
+
+import jakarta.persistence.Access;
+import jakarta.persistence.Entity;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "aop")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class AOP {
+
+	@Field("paths")
+	private List<String> paths;
+
+	public static AOP create(List<String> paths) {
+		return new AOP(paths);
+	}
+}
---
+++ src/test/java/com/hocs/server/extractor/respository/mongo/APISourceDependencyRepositoryTest.java
@@ -0,0 +1,93 @@
+package com.hocs.server.extractor.respository.mongo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.hocs.server.extractor.domain.AOP;
+import com.hocs.server.extractor.domain.API;
+import com.hocs.server.extractor.domain.APISourceDependencyInfo;
+import com.hocs.server.extractor.domain.ExceptionHandler;
+import com.hocs.server.extractor.domain.GlobalSourceDependency;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataMongoTest
+@ActiveProfiles("dev")
+class APISourceDependencyRepositoryTest {
+
+	@Autowired
+	private APISourceDependencyRepository repository;
+
+	private APISourceDependencyInfo apiDependency;
+
+	@BeforeEach
+	public void setUp() {
+		repository.deleteAll();  // 테스트 환경 초기화
+
+		// 테스트용 APIConfiguration 객체 생성
+		API testAPI = API.create("api/v1/test", "POST", Arrays.asList("path1", "path2"));
+		GlobalSourceDependency global = GlobalSourceDependency.create(
+			UUID.randomUUID().toString(),
+			AOP.create(Arrays.asList("aopPath1", "aopPath2")),
+			ExceptionHandler.create(UUID.randomUUID().toString(), List.of("exceptionHandlerPath")),
+			Arrays.asList("config1", "config2"),
+			Arrays.asList("component1", "component2")
+		);
+
+		apiDependency = APISourceDependencyInfo.create(
+			UUID.randomUUID().toString(),
+			"testUser",
+			List.of(testAPI),
+			global);
+	}
+
+	@Test
+	@DisplayName("MongoDB에 APIConfiguration 저장 테스트")
+	public void testSaveAPIConfiguration() {
+		APISourceDependencyInfo save = repository.save(apiDependency);
+
+		assertThat(save.getId()).isNotNull();
+		assertThat(save.getApiSourceDependencies().size()).isEqualTo(1);
+		assertThat(save.getGlobal().getConfiguration()).contains("config1", "config2");
+	}
+
+	@Test
+	@DisplayName("MongoDB에서 모든 APIConfiguration 조회 테스트")
+	public void testFindAllAPIConfigurations() {
+		repository.save(apiDependency);
+
+		List<APISourceDependencyInfo> apiDependencies = repository.findAll();
+
+		assertThat(apiDependencies).hasSize(1);
+		assertThat(apiDependencies.get(0).getApiSourceDependencies().get(0).getApi()).isEqualTo(
+			"api/v1/test");
+	}
+
+	@Test
+	@DisplayName("MongoDB에서 APIConfiguration ID로 조회 테스트")
+	public void testFindAPIConfigurationById() {
+		APISourceDependencyInfo save = repository.save(apiDependency);
+
+		APISourceDependencyInfo dependencies = repository.findById(save.getId())
+			.orElse(null);
+
+		assertThat(dependencies).isNotNull();
+		assertThat(dependencies.getGlobal().getComponent()).contains("component1", "component2");
+	}
+
+	@Test
+	@DisplayName("MongoDB에서 APIConfiguration 삭제 테스트")
+	public void testDeleteAPIConfiguration() {
+		APISourceDependencyInfo save = repository.save(apiDependency);
+
+		repository.deleteById(save.getId());
+
+		assertThat(repository.findById(save.getId())).isNotPresent();
+	}
+}
