225ccb5fc715f80aa2d4b5e17cf30ae0dba43f3c
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2025-03-27 16:40:00 +0900

    🛠️FIX(Test) : 깨진 테스트 수정

--- src/test/java/com/hocs/server/extractor/service/GitApiServiceTest.java
+++ src/test/java/com/hocs/server/extractor/service/GitApiServiceTest.java
@@ -1,7 +1,8 @@
 package com.hocs.server.extractor.service;
 
-import com.hocs.server.code_parser.legacy.extractor.service.GitApiService;
 import com.hocs.server.saas_platform.domain.GitRepo;
+import com.hocs.server.saas_platform.domain.GitRepoData;
+import com.hocs.server.saas_platform.service.external.git.adapter.GitApiAdapter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -12,22 +13,11 @@ class GitApiServiceTest {
 	@Test
 	@Disabled("이 테스트는 현재 비활성화됨")
 	public void getDefaultBranch(){
-		GitApiService gitApiService = new GitApiService();
-		GitRepo gitRepo = GitRepo.of("https://github.com/khoubyari/spring-boot-rest-example.git");
-		String defaultBranch = gitApiService.getDefaultBranch(gitRepo);
+		GitApiAdapter gitApiService = new GitApiAdapter();
+		GitRepoData gitRepo = GitRepoData.of("https://github.com/khoubyari/spring-boot-rest-example.git");
+		String defaultBranch = gitApiService.getDefaultBranchName(gitRepo);
 
 		Assertions.assertEquals("master", defaultBranch);
-	}
-
-	@Disabled("이 테스트는 현재 비활성화됨")
-	@Test
-	public void buildSourceCodeUrl(){
-		GitApiService gitApiService = new GitApiService();
-		GitRepo gitRepo = GitRepo.of("https://github.com/khoubyari/spring-boot-rest-example.git");
-		String sourceCodeUrl
-			= gitApiService.buildSourceCodeUrl(gitRepo, "src/main/java/com/example/demo/controller/HelloController.java");
-
-		Assertions.assertEquals(sourceCodeUrl,"https://github.com/khoubyari/spring-boot-rest-example/blob/master//src/main/java/com/example/demo/controller/HelloController.java");
 	}
 
 }
